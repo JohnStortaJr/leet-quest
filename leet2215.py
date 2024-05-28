c16503a735344dbc2a71975239f4d5c482fa2636
JohnStortaJr <reach@JohnStortaJr.com>
2024-05-28 19:47:00 -0400

    Add template for LeetCode 2215

---
+++ leet2215.py
@@ -0,0 +1,16 @@
+# LeetCode Problem 2215 -- IN PROGRESS
+# https://leetcode.com/problems/find-the-difference-of-two-arrays
+# Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+# answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+# answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+# Note that the integers in the lists may be returned in any order.
+
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+
+        return [1,2,3,4]
+    
+
+listDifference = Solution()
+
+print(listDifference.findDifference([1,2,3,4], [5,2,4,1,3]))
