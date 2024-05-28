7b5eb961cc0539356ff18c036fad204cf033569e
JohnStortaJr <reach@JohnStortaJr.com>
2024-05-28 19:39:38 -0400

    Add in progress note

--- leet1071.py
+++ leet1071.py
@@ -1,4 +1,4 @@
-# LeetCode Problem 1071
+# LeetCode Problem 1071 -- IN PROGRESS
 # https://leetcode.com/problems/greatest-common-divisor-of-strings
 # For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t
 #    (i.e., t is concatenated with itself one or more times).
