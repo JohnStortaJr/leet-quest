7cb54b9a8da0dfd8100836fb333a563e601382fe
JohnStortaJr <reach@JohnStortaJr.com>
2024-05-29 12:15:47 -0400

    Completed solution for 2215

--- leet2215.py
+++ leet2215.py
@@ -6,11 +6,34 @@
 # Note that the integers in the lists may be returned in any order.
 
 class Solution:
-    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+    def findDifference(self, nums1: list[int], nums2: list[int]) -> list[list[int]]:
+        # Some additional information about the above argument notation
+        # nums1: List[int] indicates that the passed value will be named nums1 within this function
+        # : List[int] is a tag that specifies more specifically what type this argument will be
+        # -> List[List[int]] is a tag that indicates what will be returned from this function
+        # In this case it returns a List of Lists.
 
-        return [1,2,3,4]
+        answer = []
+        answer1 = []
+        answer2 = []
+
+        for x in nums1:
+            if x not in nums2:
+                if x not in answer1:
+                    answer1.append(x)
+        
+        for x in nums2:
+            if x not in nums1:
+                if x not in answer2:
+                    answer2.append(x)
+
+        answer.append(answer1)
+        answer.append(answer2)
+
+        return answer
     
 
 listDifference = Solution()
 
-print(listDifference.findDifference([1,2,3,4], [5,2,4,1,3]))
+print(listDifference.findDifference([1,2,3], [2,4,6]))
+#print(listDifference.findDifference([1,2,3,3], [1,1,2,2]))
