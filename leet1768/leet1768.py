e3b24866ab991b9fdf6a0d170cfa56d0c8789e70
JohnStortaJr <reach@JohnStortaJr.com>
2024-05-29 15:06:40 -0400

    Adjust comments in readmes and code

--- leet1768/leet1768.py
+++ leet1768/leet1768.py
@@ -1,11 +1,4 @@
 # LeetCode problem 1768
-# https://leetcode.com/problems/merge-strings-alternately
-# You are given two strings word1 and word2.
-# Merge the strings by adding letters in alternating order, starting with word1.
-# If a string is longer than the other, append the additional letters onto the end of the merged string.
-# Return the merged string.
-
-# Paste the contents of the mergeAlternately function into LeetCode
 
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
--- leet2215/leet2215.py
+++ leet2215/leet2215.py
@@ -1,9 +1,4 @@
-# LeetCode Problem 2215 -- IN PROGRESS
-# https://leetcode.com/problems/find-the-difference-of-two-arrays
-# Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
-# answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
-# answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
-# Note that the integers in the lists may be returned in any order.
+# LeetCode Problem 2215
 
 class Solution:
     def findDifference(self, nums1: list[int], nums2: list[int]) -> list[list[int]]:
