2391f0c4691b8329de887cd3231110d3a27aaba7
JohnStortaJr <reach@JohnStortaJr.com>
2024-05-30 13:05:07 -0400

    Setup core leet0217 code

---
+++ a1-arrays_hashing/leet0217-contains-duplicate.py
@@ -0,0 +1,14 @@
+# LeetCode Problem 0217
+# https://leetcode.com/problems/contains-duplicate
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+        return True
+
+
+mySolution = Solution()
+
+print(mySolution.containsDuplicate([1,2,3,1]))
+#print(mySolution.containsDuplicate([1,2,3,4]))
+#print(mySolution.containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
