de0a834ee0d05a4e856568f03b1f7e25b2f913a9
JohnStortaJr <reach@JohnStortaJr.com>
2024-06-03 13:02:48 -0400

    Rename in progress problems

---
+++ a1-arrays_hashing/_leetM0049-group-anagrams.py
@@ -0,0 +1,12 @@
+# LeetCode Problem 0049
+# https://leetcode.com/problems/group-anagrams
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+    
+leet0049 = Solution()
+
+print(leet0049.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+print(leet0049.groupAnagrams([""]))    
+print(leet0049.groupAnagrams(["a"]))    
---
+++ a1-arrays_hashing/_leetM0128-longest-consecutive-sequence.py
@@ -0,0 +1,11 @@
+# LeetCode Problem 0128
+# https://leetcode.com/problems/longest-consecutive-sequence
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+ 
+
+leet0128 = Solution()
+
+print(leet0128.longestConsecutive([100,4,200,1,3,2]))
+print(leet0128.longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
